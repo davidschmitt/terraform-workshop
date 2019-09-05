269b4dc6fbb0d64c3f2e2f223cf6f08017c02715
David Schmitt <David_Schmitt@shi.com>
2019-09-05 18:24:24 -0400

    Adding documentation

--- steps/step50.sh
+++ steps/step50.sh
@@ -1,4 +1,9 @@
 #
-# Use that jump script Terraform created for us to jump to the internal server
+# Use that jump script Terraform created for us to jump to the private server
+#
+# Notice this proves our VPC peering is working since the Bastion host is
+# in VPC 1 and the private server is in VPC 2!
+#
+# Log out of the private server (e.g. "exit") to continue the workshop.
 #
 bash jump.sh
